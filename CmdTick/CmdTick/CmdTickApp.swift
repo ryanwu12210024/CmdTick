5f987a63ecb30aa7e32e28f343ea611ef45cd0c2
ryanwu12210024 <wrl021221@outlook.com>
2026-01-05 09:29:44 +0800

    set up Xcode env

---
+++ CmdTick/CmdTick/CmdTickApp.swift
@@ -0,0 +1,17 @@
+//
+//  CmdTickApp.swift
+//  CmdTick
+//
+//  Created by 吴睿林 on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CmdTickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CmdTick/CmdTick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CmdTick
+//
+//  Created by 吴睿林 on 1/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
